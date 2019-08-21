3807dc0a6075f8bb83341d261c796afe1ebdb0a4
yongest <1046788379@qq.com>
2019-08-21 21:14:20 +0800

    使用云函数

---
+++ cloud/miniprogram/app.js
@@ -0,0 +1,59 @@
+//app.js
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      })
+    }
+
+    this.globalData = {
+      openid:'',
+      
+    };
+    this.onGetOpenid();
+  },
+  onGetOpenid: function () {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: data => {
+      
+        this.globalData.openid = data.result.openid
+        // 
+        const db = wx.cloud.database()
+        db.collection('users').where({
+          _openid: data.result.openid,
+         
+        })
+          .get({
+            success: function (res) {
+              // res.data 是包含以上定义的两条记录的数组
+             
+              if (!res.data.length){
+              //  存储用户登录时间到数据库，openid 会自动添加上
+                console.log(data.result.openid, res)
+                db.collection('users').add({
+                data: {
+                   "login_time": [new Date()]
+                 }
+                })
+              }
+            },
+            fail(e){
+              console.log(e)
+            }
+          })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  }
+})
---
+++ cloud/miniprogram/pages/index/index.js
@@ -0,0 +1,90 @@
+// miniprogram/pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    books:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    // 1. 获取数据库引用
+    const db = wx.cloud.database()
+
+    const books = db.collection('books')
+    const me = this;
+    books.get({
+      success: function (res) {
+        // 输出 [{ "title": "The Catcher in the Rye", ... }]
+        console.log(res)
+        me.setData({
+          books:res.data
+        })
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+
+    db.collection('users').get({
+      success(data){
+        console.log(data)
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
