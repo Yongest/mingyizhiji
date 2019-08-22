bf488ce92d2a7e3b99182ca3bc8af10231317e4c
yongest <1046788379@qq.com>
2019-08-22 19:52:07 +0800

    反馈系统

--- cloud/miniprogram/app.js
+++ cloud/miniprogram/app.js
@@ -21,6 +21,8 @@ App({
   onGetOpenid: function () {
     // 调用云函数
     let me = this;
+
+   
     wx.cloud.callFunction({
       name: 'login',
       data: {},
@@ -38,16 +40,22 @@ App({
               // res.data 是包含以上定义的两条记录的数组
 
               if (!res.data.length) {
-                //  存储用户登录时间到数据库，openid 会自动添加上
-               
-                db.collection('users').add({
-                  data: {
-                    "login_time": [new Date()]
-                  }, success(data) {
-                    console.log(data,666)
-                    me.globalData.user_id = data._id
+                // 获取设备信息
+                wx.getSystemInfo({
+                  success: function (info) {
+                     //  存储用户登录时间到数据库，openid 会自动添加上
+                    db.collection('users').add({
+                      data: {
+                        "login_time": [new Date()],
+                        "mobile_info":info
+                      }, success(data) {
+                        console.log(data, 666)
+                        me.globalData.user_id = data._id
+                      }
+                    })
                   }
                 })
+               
               } else {
                 console.log(res.data, 222)
                 me.globalData.user_id = res.data[0]._id
